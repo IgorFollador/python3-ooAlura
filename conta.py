2ff3301d5edebf56d99c2725557ec92725481bb2
Igor Dallazen Follador <igor_follador@sicredi.com.br>
2022-04-28 13:54:16 -0300

    Using methods

--- conta.py
+++ conta.py
@@ -1,4 +1,3 @@
-
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
@@ -6,3 +5,15 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        if self.saldo - valor >= 0:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente!")
